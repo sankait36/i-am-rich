f5633b34a678810d84a9631aeb3cde8a7f79df87
sankait36 <sankait36@hotmail.com>
2019-06-10 16:37:37 -0500

    Icons/Images added, defaults removed

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      // A Scaffold is a basic empty frame where we can arrange our components
+      home: Scaffold(
+        // Pre-built top app bar that follows material design
+        appBar: AppBar(
+          title: Text('I Am Rich!'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
